83889b6fe8b4c854bcd79f6260eff753293cd93e
JustaJunk <tcfsh.crazer@gmail.com>
2021-11-27 23:15:54 +0800

    add constants/pricePairs.ts

---
+++ packages/react-app/src/constants/pricePairs.ts
@@ -0,0 +1,7 @@
+const pricePairs = {
+    "ETH / USD" : "eth-usd.data.eth",
+    "BTC / USD" : "btc-usd.data.eth",
+    "BNB / USD" : "bnb-usd.data.eth",
+    "LINK / USD" : "link-usd.data.eth",
+}
+export default pricePairs;
