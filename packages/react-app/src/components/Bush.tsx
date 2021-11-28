496c9e60b27e4b326b8c812d47778d62fdf3d6a6
Jeffery.YH.Lin <jeffery.yh.lin@JefferyYHLindeMacBook-Pro.local>
2021-11-29 04:28:35 +0800

    before pull

--- packages/react-app/src/components/Bush.tsx
+++ packages/react-app/src/components/Bush.tsx
@@ -1,10 +1,34 @@
-import React from 'react'
 
+import React, { useEffect } from 'react';
+import { useMoralis } from "react-moralis";
+import { META_JUNGLE_ADDRESS } from '../constants/address';
+import { openNotificationWithIcon } from '../helpers/notification';
+import { useMoralisDapp } from "../providers/MoralisDappProvider/MoralisDappProvider";
+import { MetaJungle__factory } from "../typechain";
 interface Props {
     id: number
 }
 
-const Bush = (props: Props) => {
+const Bush = ({ id }: Props) => {
+    const { walletAddress, chainId, native } = useMoralisDapp();
+    useEffect(() => {
+        const getJunglerOnBush = async () => {
+            if (chainId && walletAddress) {
+                const options = {
+                    chain: chainId,
+                    address: META_JUNGLE_ADDRESS[chainId],
+                    function_name: "getJunglerOnBush",
+                    abi: MetaJungle__factory.abi,
+                    params: { bushId: id.toString() }
+                }
+                const response = await native.runContractFunction(options);
+                console.log(response);
+            }
+        }
+    })
+    const handleHideOnBush = (junglerId: number, bushId: number) => {
+
+    }
     return (
         <div>
 
